f3b795fadda9034be489c2e172e5219e16aa6beb
Rasuljon <rasuljon@example.com>
2025-04-21 11:23:48 -0500

    Complete 80%  user settings:Google accaunt login,  Reset Password, Change Password, Email Verification, Styling updates

---
+++ .history/next-env.d_20250421111849.ts
@@ -0,0 +1,7 @@
+// next-env.d.ts
+/// <reference types="next" />
+/// <reference types="next/navigation-types/compat/navigation" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: Bu faylni tahrirlamang
+// https://nextjs.org/docs/app/api-reference/config/typescript
