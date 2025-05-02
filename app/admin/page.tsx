294652f77ff8be3a7679759d91cee0a09ea77528
Rasuljon <rasuljon@example.com>
2025-05-01 19:43:51 -0500

    fixed admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,15 +1,15 @@
-// // app/admin/page.tsx
-// import { getServerSession } from 'next-auth';
-// import { authOptions } from '@/lib/auth';
-// import { redirect } from 'next/navigation';
-// // import AdminPageClient from './AdminPageClient';
+// app/admin/page.tsx
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
+import { redirect } from 'next/navigation';
+import AdminPageClient from './AdminPageClient';
 
-// export default async function AdminPage() {
-//   const session = await getServerSession(authOptions);
+export default async function AdminPage() {
+  const session = await getServerSession(authOptions);
 
-//   if (!session || session.user?.role !== 'admin') {
-//     redirect('/login');
-//   }
+  if (!session || session.user?.role !== 'admin') {
+    redirect('/login');
+  }
 
-//   return <AdminPageClient />;
-// }
+  return <AdminPageClient />;
+}
